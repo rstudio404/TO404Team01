cbc033b0fdcb5bf1a993ee3bb3490a3e199c3a75
Josh Hartman <hartjosh@umich.edu>
2020-12-13 20:16:44 -0500

    with code to initialize sample

---
+++ server.R
@@ -0,0 +1,56 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+#citibikesample <- read.csv("samplecsv.csv")
+#library("lubridate"); library("chron");library("timeDate")
+
+#getting relevant vectors from sample data
+#datafortime <- citibikesample[,c(2:4,6:8,10:13)]
+
+#ymdhms format
+#datafortime$starttime <- ymd_hms(datafortime$starttime)
+#datafortime$stoptime <- ymd_hms(datafortime$stoptime)
+#datafortime$roundedhour <- floor_date(datafortime$starttime, "hour")
+
+#count number of times per usertype per hour that a start station is used
+#startstationbyroundedhour <- count(datafortime, start.station.id, usertype, roundedhour)
+
+#count number of times per usertype per hour that a end station is used
+#endstationbyroundedhour <- count(datafortime, end.station.id, usertype, roundedhour)
+
+#combine df's by station id and month
+#combinedcountbyroundedhour <- merge(startstationbyroundedhour, endstationbyroundedhour, by.x = c("start.station.id","usertype","roundedhour"), by.y = c("end.station.id","usertype","roundedhour"))
+
+#find surplus(deficit) and make a new vector
+#combinedcountbyroundedhour$dif <- c(combinedcountbyroundedhour$n.y - combinedcountbyroundedhour$n.x)
+combinedcountbyroundedhour <- read.csv("Rshinydynamicpricing.csv")
+library(shiny)
+library(ggplot2)
+
+shinyServer(function(input, output) {
+   
+  pricing <- reactive({
+    
+    combinedcountbyroundedhour$pricing <- ifelse(input$MaxPrice*(1-pnorm(combinedcountbyroundedhour$dif, mean(combinedcountbyroundedhour$dif, na.rm = TRUE), sd(combinedcountbyroundedhour$dif, na.rm = TRUE))) < input$MinPrice, input$MinPrice,input$MaxPrice*(1-pnorm(combinedcountbyroundedhour$dif, mean(combinedcountbyroundedhour$dif, na.rm = TRUE), sd(combinedcountbyroundedhour$dif, na.rm = TRUE))))
+    
+  })
+  
+  #specify output plot - shows line graph of surplus(deficit) to pricing reactive variable
+  output$results <- renderPlot({
+    ggplot(combinedcountbyroundedhour,aes(x=dif, y=pricing()))+geom_line(color = "darkgreen")
+  })
+  
+  #specify output data table - shows pricing for changing price inputs
+  output$list <- renderDataTable({
+    cbind(pricing(), combinedcountbyroundedhour)
+    
+  })
+})
+  
+
